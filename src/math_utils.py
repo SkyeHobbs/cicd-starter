3368f91cb2346eb85ca692d01215d3637cdf020c
Eric Lloyd <eric.lloyd@seattlecolleges.edu>
2026-01-14 15:35:16 -0800

    initial commit

---
+++ src/math_utils.py
@@ -0,0 +1,6 @@
+def add(a: int, b: int) -> int:
+    """
+    Return the sum of two integers.
+    Students will deliberately break this, then fix it to make CI pass.
+    """
+    return a + b
---
+++ tests/test_math_utils.py
@@ -0,0 +1,5 @@
+from src.math_utils import add
+
+
+def test_add_basic():
+    assert add(2, 3) == 5
